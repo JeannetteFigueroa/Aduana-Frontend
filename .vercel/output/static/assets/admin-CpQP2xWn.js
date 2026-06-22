570c9f1e1871d8a0276101018d3cf44a7e3f9532
Jeannette Alejandra Figueroa Diaz <jeannette.figueroa.diaz.iccp@gmail.com>
2026-06-21 20:52:04 -0400

    correción en estructura

--- .vercel/output/static/assets/admin-CpQP2xWn.js
+++
@@ -1,1 +0,0 @@
-import{j as o,O as t}from"./index-Yb9mHT9q.js";import{P as e}from"./protected-route-DhxOL1VK.js";const m=()=>o.jsx(e,{requiredRole:"ADMIN",children:o.jsx(t,{})});export{m as component};
--- .vercel/output/static/assets/mock-data-9ZqpoxCg.js
+++
@@ -1,1 +0,0 @@
-const s=[],a=[],o=[],t=[];export{t as a,a as b,o as p,s as v};
